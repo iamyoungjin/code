b7e4f57def0a17325125deb87ec70fb5afefd97f
iamyoungjin <nnna08@likelion.org>
2021-11-20 21:47:56 -0800

    add: 1463

--- 1463.py
+++ 1463.py
@@ -1,1 +1,14 @@
-#hard....
+
+
+
+if __name__=="__main__":
+n = int(input())
+d = [0]*(n+1)
+d[1] = 0
+for i in range(2, n+1):
+    d[i] = d[i-1] + 1      #1을 왜 먼저 실행하는지 이해가 되지 않음...
+    if i%2 == 0 and d[i] > d[i//2] + 1:
+        d[i] = d[i//2] + 1
+    if i%3 == 0 and d[i] > d[i//3] + 1:
+        d[i] = d[i//3] + 1
+print(d[n])
