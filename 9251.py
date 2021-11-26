2009ed6d615e1d67df8045bf571b2224954b9b0e
iamyoungjin <nnna08@likelion.org>
2021-11-26 13:55:44 -0800

    add: 9251

---
+++ 9251.py
@@ -0,0 +1,20 @@
+#LCS
+
+if __name__=='__main__':
+	s1 = list(input())
+	s2 = list(input())
+	l1 = len(s1)
+	l2 = len(s2)
+	dp = [[0]*(l2 + 1) for _ in range(l1+1)]
+	print(dp)
+
+	for i in range(1,l1 + 1) :
+	    for j in range(1,l2 + 1) :
+	        if s1[i-1] == s2[j-1] :
+	            dp[i][j] = dp[i-1][j-1] + 1
+	        else :
+	            dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+
+	print(dp)
+
+	print(dp[l1][l2])
